407dc94badb7ba7ea620e65f558b2e9e44174c3c
Eduardo Fernandes <ducabruno09@hotmail.com>
2024-01-04 16:00:36 -0400

    test: add test for creating todo when date is in the past or future with sinonjs

--- test/todoService.test.js
+++ test/todoService.test.js
@@ -1,8 +1,9 @@
-import { describe, it, beforeEach, before, after } from "node:test";
+import { describe, it, beforeEach, afterEach, before, after } from "node:test";
 import assert from "node:assert";
 import crypto from "node:crypto";
 import Todo from "../src/todo.js";
 import TodoService from "../src/todoService.js";
+import sinon from "sinon";
 
 describe("TodoService", () => {
   describe("#list", () => {
@@ -47,8 +48,10 @@ describe("TodoService", () => {
     /** @type {TodoService} */
     let _todoService;
     let _dependencies;
+    /** @type {sinon.SinonSandbox} */
+    let _sandbox;
 
-    const mockDatabase = [
+    const mockCreateResult = [
       {
         text: "I must plan my trip to Europe",
         when: new Date("2021-03-22T00:00:00.000Z"),
@@ -60,7 +63,10 @@ describe("TodoService", () => {
     before(() => {
       const DEFAULT_ID = "0001";
       crypto.randomUUID = () => DEFAULT_ID;
+      _sandbox = sinon.createSandbox();
     });
+
+    afterEach(() => _sandbox.restore());
 
     after(async () => {
       crypto.randomUUID = (await import("node:crypto")).randomUUID;
@@ -69,7 +75,7 @@ describe("TodoService", () => {
     beforeEach((context) => {
       _dependencies = {
         todoRepository: {
-          create: context.mock.fn(async () => mockDatabase),
+          create: context.mock.fn(async () => mockCreateResult),
         },
       };
 
@@ -77,7 +83,7 @@ describe("TodoService", () => {
     });
 
     it("should not create a todo item with invalid data", async () => {
-      const expectedDatabase = mockDatabase;
+      const expectedDatabase = mockCreateResult;
       const newTodo = new Todo({ text: "new todo", when: "today" });
       const expectedTodo = {
         error: {
@@ -94,15 +100,59 @@ describe("TodoService", () => {
       const result = _todoService.create(newTodo);
 
       assert.ok("error" in result);
-      assert.deepStrictEqual(mockDatabase, expectedDatabase);
+      assert.deepStrictEqual(mockCreateResult, expectedDatabase);
       assert.deepStrictEqual(
         JSON.stringify(result),
         JSON.stringify(expectedTodo)
       );
     });
 
-    it.todo("should be able to create a todo", () => {
-      // assert.strictEqual(1, 2);
+    it("should create todo with late status when property is further than today", async () => {
+      const properties = {
+        text: "I must plan my trip to Europe",
+        when: new Date("2020-12-01 12:00:00 GMT-0"),
+      };
+
+      const input = new Todo(properties);
+      const expected = {
+        ...properties,
+        status: "late",
+        id: "0001",
+      };
+
+      const today = new Date("2020-12-02");
+      _sandbox.useFakeTimers(today.getTime());
+
+      await _todoService.create(input);
+
+      const fnMock = _dependencies.todoRepository.create.mock;
+      assert.strictEqual(fnMock.callCount(), 1);
+
+      assert.deepStrictEqual(fnMock.calls[0].arguments[0], expected);
+    });
+
+    it("should create todo with pending status when property is in the past", async () => {
+      const properties = {
+        text: "I must plan my trip to Europe",
+        when: new Date("2020-12-02 12:00:00 GMT-0"),
+      };
+
+      const input = new Todo(properties);
+      const expected = {
+        ...properties,
+        status: "pending",
+        id: "0001",
+      };
+
+      const today = new Date("2020-12-01");
+      _sandbox.useFakeTimers(today.getTime());
+
+      await _todoService.create(input);
+
+      const fnMock = _dependencies.todoRepository.create.mock;
+      assert.strictEqual(fnMock.callCount(), 1);
+
+      assert.deepStrictEqual(fnMock.calls[0].arguments[0], expected);
     });
   });
 });
